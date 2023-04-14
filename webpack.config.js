f626da5ec2f5e882922a6a6f1bbc7904bb8c4d37
pchekushkin <pchekushkin@clink.tech>
2023-04-14 15:11:22 +0400

    Оптимизация импортов

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,11 @@ module.exports = {
             '@': path.resolve(__dirname, 'src')
         }
     },
+    optimization: {
+        splitChunks: {
+            chunks: "all",
+        }
+    },
     plugins: [
         new HTMLWebpackPlugin({
             template: "./index.html"
